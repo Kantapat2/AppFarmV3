6ea9b306a4e3acc6857578e39e8ffa68fc2a2a54
Com_Arm <kantawits62@nu.ac.th>
2021-06-30 15:24:29 +0700

    EditBugPromo

--- lib/screen/promotions/components/Body_Botton.dart
+++ lib/screen/promotions/components/Body_Botton.dart
@@ -3,43 +3,39 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:SumApp_1/Constants/constant.dart';
 
 class Buttonss extends StatelessWidget {
-  
   int countlist = 4;
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         Container(
-          decoration: new BoxDecoration(
-              color: kPrimaryBgColor),
+          decoration: new BoxDecoration(color: kPrimaryBgColor),
         ),
-
-
-
         SingleChildScrollView(
-          physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+          physics: const BouncingScrollPhysics(
+              parent: AlwaysScrollableScrollPhysics()),
           child: Column(
             children: <Widget>[
-              Container(height:10,width: 10,
-              ),  
-
-              if(countlist !=0)...[
-              for(int i=0;i<=countlist;i++)
-              Buttons(
-                image: "images/Ex_pro.jpg",
-                title:
-                    "ปั้นสิบไส้ปลา ซื้อครบ 10 ชิ้น ลด 10%",
-                press: (){print('XD');},
+              Container(
+                height: 10,
+                width: 10,
               ),
-
-              ]
-              else...
-              [
+              if (countlist != 0) ...[
+                for (int i = 0; i <= countlist; i++)
+                  Buttons(
+                    image: "images/Ex_pro.jpg",
+                    title: "ปั้นสิบไส้ปลา ซื้อครบ 10 ชิ้น ลด 10%",
+                    press: () {
+                      print('XD');
+                    },
+                  ),
+              ] else ...[
                 Container(
-                  height: MediaQuery.of(context).size.height*0.8,
+                  height: MediaQuery.of(context).size.height * 0.8,
                   //color: Colors.teal,
                   child: Center(
-                    child: Text('กิจกรรมจะจัดในเร็วๆนี้',style: TextStyle(fontSize: 25)),
+                    child: Text('กิจกรรมจะจัดในเร็วๆนี้',
+                        style: TextStyle(fontSize: 25)),
                   ),
                 )
               ]
@@ -52,7 +48,8 @@ class Buttonss extends StatelessWidget {
 }
 
 class Buttons extends StatelessWidget {
-  const Buttons({
+  Buttons({
+    //const
     Key key,
     this.image,
     this.title,
@@ -61,8 +58,12 @@ class Buttons extends StatelessWidget {
   final String image, title;
   final Function press;
 
+  var size;
+
   @override
   Widget build(BuildContext context) {
+    size = MediaQuery.of(context).size;
+    //print('width ${size.width}');
     return Container(
       margin: EdgeInsets.only(left: 20, top: 10, bottom: 10, right: 20),
       padding: EdgeInsets.only(left: 20, top: 20, bottom: 20, right: 20),
@@ -70,7 +71,7 @@ class Buttons extends StatelessWidget {
       width: 500,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(22),
-        color: Colors.white,
+        color: Colors.white, //
         boxShadow: [
           BoxShadow(
               spreadRadius: 0,
@@ -96,15 +97,18 @@ class Buttons extends StatelessWidget {
                   ),
                 ),
               ),
-              SizedBox(
-                width: 200,
-                height: 140,
-                child: AutoSizeText(
-                  '$title',
-                  style: TextStyle(
-                    height: 1.6,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+              Container(
+                //color: Colors.grey,
+                child: SizedBox(
+                  width: size.width * 0.4, //200 Error
+                  height: 140,
+                  child: AutoSizeText(
+                    '$title',
+                    style: TextStyle(
+                      height: 1.6,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
